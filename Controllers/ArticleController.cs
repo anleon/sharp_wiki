c411d987abd301ee3e17b0f3fe428cfb2fda1d2b
unknown <mnordin@.(none)>
2010-10-28 13:29:54 +0200

    Merging before breaking

---
+++ Controllers/ArticleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Wiki_test.Controllers
+{
+    public class ArticleController : Controller
+    {
+        //
+        // GET: /Article/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/ArticleModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Wiki_test.Models
+{
+    public class ArticleModels
+    {
+
+    }
+}
